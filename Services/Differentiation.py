f8288a00ba981793e38f981bcfe76e77c788dd98
JohnSalas8 <johnsalaslp@gmail.com>
2018-05-29 08:22:09 -0500

    Agregando servicios

---
+++ Services/Differentiation.py
@@ -0,0 +1,35 @@
+import math
+
+expression = ''
+
+def differentation_to_right (exp, x, h):
+    """ exp -> Es f(x) de la integral
+        x   -> Es el punto en el que se va evaluar
+        h   -> Es el intervalo
+    """
+    expression = exp.replace(' ','')
+
+    if not len(expression):
+        raise Exception('El tamanio de la expresion es cero.')
+    else:
+        x = x+h
+        new_exp = expression.replace('x','('+str(x)+')')
+        f_xi1 = eval(new_exp)
+        print 'f('+str(x)+') =', new_exp, '=', f_xi1
+
+        x = x-2*h
+        new_exp = expression.replace('x','('+str(x)+')')
+        f_xi2 = eval(new_exp)
+        print 'f('+str(x)+') =', new_exp, '=', f_xi2
+
+        fp_xi = (f_xi1 - f_xi2) / (2*h)
+
+        print ''
+        print "f'("+str(x+h)+") =", fp_xi
+
+def main():
+    exp = 'math.e**(-2*x+1) * math.cos(7*x-8) * (6*x**2-1)'
+    differentation_to_right(exp, 1, 0.01)
+
+if __name__ == '__main__':
+    main()
---
+++ Services/DifferentiationToLeft.py
@@ -0,0 +1,39 @@
+import math
+
+expression = ''
+
+def differentation_to_right (exp, x, h):
+    """ exp -> Es f(x) de la integral
+        x   -> Es el punto en el que se va evaluar
+        h   -> Es el intervalo
+    """
+    expression = exp.replace(' ','')
+
+    if not len(expression):
+        raise Exception('El tamanio de la expresion es cero.')
+    else:
+        new_exp = expression.replace('x','('+str(x)+')')
+        f_xi = eval(new_exp)
+        print 'f('+str(x)+') =', new_exp, '=', f_xi
+
+        x = x-h
+        new_exp = expression.replace('x','('+str(x)+')')
+        f_xi1 = eval(new_exp)
+        print 'f('+str(x)+') =', new_exp, '=', f_xi1
+
+        x = x-h
+        new_exp = expression.replace('x','('+str(x)+')')
+        f_xi2 = eval(new_exp)
+        print 'f('+str(x)+') =', new_exp, '=', f_xi2
+
+        fp_xi = (f_xi2 - 4*f_xi1 +3*f_xi) / (2*h)
+
+        print ''
+        print "f'("+str(x+h*2)+") =", fp_xi
+
+def main():
+    exp = 'math.e**(-2*x+1) * math.cos(7*x-8) * (6*x**2-1)'
+    differentation_to_right(exp, 1, 0.01)
+
+if __name__ == '__main__':
+    main()
