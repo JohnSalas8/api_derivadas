c5ef81abb7a719bef63cb558979a5e1b0dc2cbf2
JohnSalas8 <johnsalaslp@gmail.com>
2018-05-29 09:35:36 -0500

    Servicio terminado de dif., Izq

--- Services/DifferentiationToLeft.py
+++ Services/DifferentiationToLeft.py
@@ -1,39 +1,61 @@
 import math
+import json
+import ExpMath
 
 expression = ''
 
-def differentation_to_right (exp, x, h):
-    """ exp -> Es f(x) de la integral
-        x   -> Es el punto en el que se va evaluar
-        h   -> Es el intervalo
-    """
-    expression = exp.replace(' ','')
+class DifferentiationToLeft:
+    def __init__(self):
+        pass
+    
+    def differentation_to_left (self, exp, x, h):
+        """ exp -> Es f(x) de la integral
+            x   -> Es el punto en el que se va evaluar
+            h   -> Es el intervalo
+        """
+        
+        expression = exp
 
-    if not len(expression):
-        raise Exception('El tamanio de la expresion es cero.')
-    else:
-        new_exp = expression.replace('x','('+str(x)+')')
-        f_xi = eval(new_exp)
-        print 'f('+str(x)+') =', new_exp, '=', f_xi
+        for i in ExpMath.exp_math:
+            expression = expression.replace(i[0], i[1])
 
-        x = x-h
-        new_exp = expression.replace('x','('+str(x)+')')
-        f_xi1 = eval(new_exp)
-        print 'f('+str(x)+') =', new_exp, '=', f_xi1
+        vjson = {}
 
-        x = x-h
-        new_exp = expression.replace('x','('+str(x)+')')
-        f_xi2 = eval(new_exp)
-        print 'f('+str(x)+') =', new_exp, '=', f_xi2
+        vjson['exp'] = exp
+        vjson['x'] = x
+        vjson['h'] = h
 
-        fp_xi = (f_xi2 - 4*f_xi1 +3*f_xi) / (2*h)
+        vjson['procedure'] = ''
 
-        print ''
-        print "f'("+str(x+h*2)+") =", fp_xi
+        if not len(expression):
+            raise Exception('El tamanio de la expresion es cero.')
+        else:
+            new_exp = expression.replace('x','('+str(x)+')')
+            f_xi = eval(new_exp)
+            vjson['procedure'] += 'f(' + str(x) + ') = ' + new_exp + ' = ' + str(f_xi)
 
-def main():
-    exp = 'math.e**(-2*x+1) * math.cos(7*x-8) * (6*x**2-1)'
-    differentation_to_right(exp, 1, 0.01)
+            x = x-h
+            new_exp = expression.replace('x','('+str(x)+')')
+            f_xi1 = eval(new_exp)
+            vjson['procedure'] += 'f(' + str(x) + ') = ' + new_exp + ' = ' + str(f_xi1)
+
+            x = x-h
+            new_exp = expression.replace('x','('+str(x)+')')
+            f_xi2 = eval(new_exp)
+            vjson['procedure'] += 'f(' + str(x) + ') = ' + new_exp + ' = ' + str(f_xi2)
+
+            vjson['procedure'] = vjson['procedure'].replace('math.', '')
+            vjson['procedure'] = vjson['procedure'].replace('**', '^')
+
+            fp_xi = (f_xi2 - 4*f_xi1 +3*f_xi) / (2*h)
+
+            vjson['result'] = ''
+            vjson['result'] +=  "f'(" + str(x+h*2) + ") = (" + str(f_xi2)  + ' 4*' + str(f_xi1) + '3*' + str(f_xi) + ') / (' + '2*' + str(h) + ') = ' + str(fp_xi)
+
+            return json.dumps(vjson, indent=4)
 
 if __name__ == '__main__':
-    main()
+    print DifferentiationToLeft().differentation_to_left (
+        'e^(-2*x+1)*cos(7*x-8)*(6*x^2-1)',
+        1, 0.01
+    )
