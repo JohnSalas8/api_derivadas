6c366dd818fad1834bdfb6a2ea08da71d9b676e5
JohnSalas8 <johnsalaslp@gmail.com>
2018-05-29 09:42:21 -0500

    Servicio terminado de dif., Der

--- Services/DifferentiationToRight.py
+++ Services/DifferentiationToRight.py
@@ -1,35 +1,53 @@
 import math
+import json
+import ExpMath
 
-expression = ''
+
+class DifferentiationToRight:
+    def __init__(self):
+        pass
+        
 
 def differentation_to_right (exp, x, h):
     """ exp -> Es f(x) de la integral
         x   -> Es el punto en el que se va evaluar
         h   -> Es el intervalo
     """
-    expression = exp.replace(' ','')
+    expression = exp
+
+    for i in ExpMath.exp_math:
+        expression = expression.replace(i[0], i[1])
+
+    vjson = {}
+
+    vjson['exp'] = exp
+    vjson['x'] = x
+    vjson['h'] = h
+
+    vjson['procedure'] = ''
+
+    vjson['result'] = ''
 
     if not len(expression):
         raise Exception('El tamanio de la expresion es cero.')
     else:
         new_exp = expression.replace('x','('+str(x)+')')
         f_xi = eval(new_exp)
-        print 'f('+str(x)+') =', new_exp, '=', f_xi
+        vjson['procedure'] += 'f(' + str(x) + ') =' + new_exp + ' = ' + str(f_xi)
 
         x = x+h
         new_exp = expression.replace('x','('+str(x)+')')
         f_xi1 = eval(new_exp)
-        print 'f('+str(x)+') =', new_exp, '=', f_xi1
+        vjson['procedure'] += 'f(' + str(x) + ') = ' + new_exp + ' = ' + str(f_xi1)
 
         x = x+h
         new_exp = expression.replace('x','('+str(x)+')')
         f_xi2 = eval(new_exp)
-        print 'f('+str(x)+') =', new_exp, '=', f_xi2
+        vjson['procedure'] += 'f(' + str(x) + ') = ' + new_exp + ' = ' + str(f_xi2)
 
         fp_xi = (-f_xi2 + 4*f_xi1 -3*f_xi) / (2*h)
 
-        print ''
-        print "f'("+str(x-h*2)+") =", fp_xi
+        vjson['result'] += "f'(" + str(x-h*2) + ") = ( -" + str(f_xi2)  + ' + 4*' + str(f_xi1) + '- 3*' + str(f_xi) + ') / (' + '2*' + str(h) + ' ) = ' + str(fp_xi)
 
 def main():
     exp = 'math.e**(-2*x+1) * math.cos(7*x-8) * (6*x**2-1)'
